ebf9ca55b9e33359b5f344d2e5b1e09c92c16fbc
malar <88694043+malarrimsys@users.noreply.github.com>
2022-01-19 12:39:27 -0800

    Updated class

--- src/test/java/test/Sample.java
+++ src/test/java/test/Sample.java
@@ -3,16 +3,25 @@ package test;
 import java.io.File;
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -21,6 +30,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class Sample {
 	public WebDriver driver;
 	
+	@SuppressWarnings("rawtypes")
 	@Test
 	public void sample() throws InterruptedException
 	
@@ -44,9 +54,19 @@ public class Sample {
 		Thread.sleep(4000);
 		System.out.println("*************************** "+driver.getTitle());
 		//TakesScreenshot scrShot =((TakesScreenshot)driver);
-		WebElement username =driver.findElement(By.id("username"));
+	
+		Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+				.withTimeout(Duration.ofSeconds(100))
+		        .pollingEvery(Duration.ofMillis(600)).ignoring(NoSuchElementException.class);
+		WebElement foo= (WebElement) wait.until(new Function<WebDriver,WebElement>() {
+			public WebElement apply(WebDriver driver) {
+			return driver.findElement(By.id("username"));
+			}});
+//		WebDriverWait web_element = new WebDriverWait(driver,100); 
+//		web_element.until(ExpectedConditions.visibilityOfElementLocated(By.id("username")));
+		//WebElement username =driver.findElement(By.id("username"));
 		WebElement pwd =driver.findElement(By.id("password"));
-		username.sendKeys("surgical@yopmail.com");
+		foo.sendKeys("surgical@yopmail.com");
 		pwd.sendKeys("Surgical2@123456");
 //		js.executeScript("document.getElementById('username').value='surgical@yopmail.com';" );
 //		js.executeScript("document.getElementById('password').value='Surgical2@123456';" );
