1916f5b0876646eaa015b1f84280958a41321f6e
malar <88694043+malarrimsys@users.noreply.github.com>
2022-01-18 16:55:52 -0800

    adding chromeoptions for jenkins

--- src/test/java/test/Sample.java
+++ src/test/java/test/Sample.java
@@ -2,6 +2,7 @@ package test;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -16,8 +17,15 @@ public class Sample {
 	{
 		WebDriverManager.chromedriver().setup();
 		System.getProperty("webdriver.chrome.driver", "chromedriver");
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("headless");
+		options.addArguments("disable-infobars"); 
+		options.addArguments("--disable-extensions");
+		options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("--no-sandbox"); 
+
 		System.out.println("Jenkins Build Run");
-		WebDriver driver =new ChromeDriver();
+		WebDriver driver =new ChromeDriver(options);
 		driver.get("https://www.google.com/");
 		System.out.println(driver.getTitle());
 	}
