3984a9dc021d5e8b80e08a152bb7c899828b571c
malar <88694043+malarrimsys@users.noreply.github.com>
2022-01-18 16:26:58 -0800

    adding dependency on the pom.xml

--- src/test/java/test/Sample.java
+++ src/test/java/test/Sample.java
@@ -1,16 +1,25 @@
 package test;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 
 public class Sample {
 
 	@Test
 	public void sample()
+	
+	
 	{
+		WebDriverManager.chromedriver().setup();
+		System.getProperty("webdriver.chrome.driver", "chromedriver");
 		System.out.println("Jenkins Build Run");
-		System.out.println("==================");
-		System.out.println("Jenkins Build Run 1.0");
+		WebDriver driver =new ChromeDriver();
+		driver.get("https://www.google.com/");
+		System.out.println(driver.getTitle());
 	}
 
 }
