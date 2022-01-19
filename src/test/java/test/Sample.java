9899804ebf477894cd5f07016390417bc7dc7626
malar <88694043+malarrimsys@users.noreply.github.com>
2022-01-18 18:32:16 -0800

    updated the test

--- src/test/java/test/Sample.java
+++ src/test/java/test/Sample.java
@@ -19,7 +19,7 @@ public class Sample {
 	
 	{
 		WebDriverManager.chromedriver().setup();
-		System.getProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+		System.getProperty("webdriver.chrome.driver", "/bin/chromedriver");
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("headless");// headless -> no browser window. needed for jenkins
 		options.addArguments("disable-infobars"); // disabling infobars
