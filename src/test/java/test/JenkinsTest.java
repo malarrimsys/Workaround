bcc712b50084cfe95d0bb7b656e7bdc1378a164d
malar <88694043+malarrimsys@users.noreply.github.com>
2022-01-21 05:49:53 -0800

    Updated class headless

--- src/test/java/test/JenkinsTest.java
+++ src/test/java/test/JenkinsTest.java
@@ -41,12 +41,12 @@ public class JenkinsTest {
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--no-sandbox"); // Bypass OS security model
 		options.setHeadless(true);
-		//options.addArguments("--headless");// headless -> no browser window. needed for jenkins
-		options.addArguments("disable-infobars"); // disabling infobars
-		options.addArguments("--disable-extensions");// disabling extensions
-		options.addArguments("--disable-dev-shm-usage");// overcome limited resource problems
-		
-		options.addArguments("--disable-features=VizDisplayCompositor");
+//		//options.addArguments("--headless");// headless -> no browser window. needed for jenkins
+//		options.addArguments("disable-infobars"); // disabling infobars
+//		options.addArguments("--disable-extensions");// disabling extensions
+//		options.addArguments("--disable-dev-shm-usage");// overcome limited resource problems
+//		
+//		options.addArguments("--disable-features=VizDisplayCompositor");
 		System.out.println("Jenkins Build Run");
 		 driver =new ChromeDriver(options);
 		
